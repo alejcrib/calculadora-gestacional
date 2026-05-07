9d5a255beecb8866975f36b91388000e90a8e9c7
Alexandre Ribeiro <alejcrib@live.com>
2026-05-06 22:18:43 -0300

    Atualiza para versão React

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export default function CalculadoraGestacional() {
   const [dataUltimaMenstruacao, setDataUltimaMenstruacao] = useState("");
